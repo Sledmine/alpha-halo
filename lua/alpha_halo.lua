6f04ad765f551341da8d8dc207fca39a9f6c4c3f
Sledmine <40512223+Sledmine@users.noreply.github.com>
2024-12-20 04:08:15 -0600

    update luna module, update to new balltze lifecycle, fix chimera compatibility

--- lua/alpha_halo.lua
+++ lua/alpha_halo.lua
@@ -1,44 +1,41 @@
 local balltze = Balltze
 local engine = Engine
+package.preload["luna"] = nil
+package.loaded["luna"] = nil
 require "luna"
 
 local main
 local loadWhenIn = {"alpha_halo"}
 
-local function isMultiplayerMap(mapName)
-    for _, map in pairs(loadWhenIn) do
-        if mapName == map or mapName == map .. "_dev" then
-            return true
-        end
-    end
-    return false
-end
+loadWhenIn = table.extend(loadWhenIn, table.map(loadWhenIn, function(map)
+    return map .. "_dev"
+end))
+
+DebugMode = false
 
 function PluginMetadata()
     return {
         name = "Alpha Halo Firefight",
         author = "El Dream Team",
         version = "1.0.0",
-        targetApi = "1.0.0-rc.1",
-        reloadable = true
+        targetApi = "1.0.0",
+        reloadable = true,
+        maps = loadWhenIn
     }
 end
 
-function PluginInit()
-    logger = balltze.logger.createLogger("Alpha Halo")
-    logger:muteIngame(false)
+local function loadChimeraCompatibility()
+    -- Load Chimera compatibility
+    for k, v in pairs(balltze.chimera) do
+        if not k:includes "timer" and not k:includes "execute_script" and
+            not k:includes "set_callback" then
+            _G[k] = v
+        end
+    end
+    server_type = engine.netgame.getServerType()
 
     -- Replace Chimera functions with Balltze functions
-    execute_script = engine.hsc.executeScript
-    write_bit = function(address, bit, value)
-        local byte = read_byte(address)
-        if value then
-            byte = byte | (1 << bit)
-        else
-            byte = byte & ~(1 << bit)
-        end
-        write_byte(address, byte)
-    end
+    write_bit = balltze.memory.writeBit
     write_byte = balltze.memory.writeInt8
     write_word = balltze.memory.writeInt16
     write_dword = balltze.memory.writeInt32
@@ -52,47 +49,24 @@ function PluginInit()
             write_byte(address, 0)
         end
     end
-
-    -- Imitate map lifecycle for scripts as in Chimera
-    -- TODO Replace this when Balltze allows per map plugin load
-    balltze.event.mapLoad.subscribe(function(event)
-        if event.time == "after" then
-            local currentMap = event.context:mapName()
-            if isMultiplayerMap(currentMap) then
-                logger:debug("Map loaded: {}", currentMap)
-                if not main then
-                    logger:info("loading main")
-                    main = require "alpha_halo.main"
-                end
-            else
-                if main then
-                    main.unload()
-                    package.loaded["alpha_halo.main"] = nil
-                    for k, v in pairs(package.loaded) do
-                        if k:startswith "alpha_halo" then
-                            package.loaded[k] = nil
-                        end
-                    end
-                    main = nil
-                end
-            end
-        end
-    end)
+    execute_script = engine.hsc.executeScript
 end
 
 function PluginLoad()
-    -- Load Chimera compatibility
-    for k, v in pairs(balltze.chimera) do
-        if not k:includes "timer" and not k:includes "execute_script"  and not k:includes "set_callback" then
-            _G[k] = v
-        end
-    end
+    logger = balltze.logger.createLogger("Alpha Halo")
+    logger:muteDebug(not DebugMode)
 
-    if isMultiplayerMap(engine.map.getCurrentMapHeader().name) then
-        if not main then
-            main = require "alpha_halo.main"
+    loadChimeraCompatibility()
+
+    balltze.event.tick.subscribe(function(event)
+        if event.time == "before" then
+            if not main then
+                main = require "alpha_halo.main"
+            end
         end
-    end
+    end)
+
+    return true
 end
 
 function PluginUnload()
