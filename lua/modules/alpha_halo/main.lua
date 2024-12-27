75b400c44ecefcaba1dd9aa32de7439b72729898
markmcfuzz <magecape10@gmail.com>
2024-12-26 22:09:08 -0600

    updated main

--- lua/modules/alpha_halo/main.lua
+++ lua/modules/alpha_halo/main.lua
@@ -1,7 +1,6 @@
-local blam = require "blam"
 local balltze = Balltze
 local engine = Engine
-require "luna"
+DebugMode = false
 local dispatchScripts = require "script".dispatch
 math.randomseed(os.time())
 --logger = balltze.logger.createLogger("Alpha Halo")
@@ -9,12 +8,8 @@ math.randomseed(os.time())
 --Project modules
 local firefightManager = require "alpha_halo.firefightManager"
 local healthManager = require "alpha_halo.gameplay_core.healthManager"
+local vehiclePositionLoader = require "alpha_halo.core"
 --local skullsManager = require "alpha_halo.gameplay_core.skullsManager"
-
-function OnMapLoad()
-    firefightManager.onMapLoad()
-    healthManager.onMapLoad()
-end
 
 -- THIS IS PROBABLY NOT ACCURATE, BUT WORKS
 -- TRUST ME, I'M SLED DA FOKIN GOAT
@@ -27,9 +22,17 @@ function getNodePosition(address)
     return {x = x, y = y, z = z}
 end
 
+function OnMapLoad()
+    firefightManager.onMapLoad()
+    healthManager.onMapLoad()
+end
+
 local isLoaded = false
 
 function OnTick()
+    firefightManager.EachTick()
+    healthManager.EachTick()
+    vehiclePositionLoader.vehiclePositionLoader()
     math.randomseed(engine.core.getTickCount())
     dispatchScripts()
     if not isLoaded then
@@ -37,57 +40,6 @@ function OnTick()
         OnMapLoad()
         return
     end
-    for objectHandle in pairs(blam.getObjects()) do
-        local object = blam.getObject(objectHandle)
-        if object and object.class == blam.objectClasses.vehicle then
-            local objectTag = blam.getTag(object.tagId)
-            if objectTag then
-                --if objectTag.path:includes("ghost") then
-                if objectTag.path:includes("covenant_spirit") then
-                    local vehicle = blam.vehicle(get_object(objectHandle))
-                    if vehicle then
-                        --execute_script("cls")
-                        local mainNode = getNodePosition(get_object(objectHandle) + 0x5B8)
-                        local absoluteNodeX = mainNode.x + (vehicle.x - mainNode.x)
-                        local absoluteNodeY = mainNode.y + (vehicle.y - mainNode.y)
-                        local absoluteNodeZ = mainNode.z + (vehicle.z - mainNode.z)
-                        --[[console_out(mainNode.x)
-                        console_out(mainNode.y)
-                        console_out(mainNode.z)
-                        console_out("-----------")
-                        console_out(absoluteNodeX)
-                        console_out(absoluteNodeY)
-                        console_out(absoluteNodeZ)]]
-                        vehicle.x = mainNode.x
-                        vehicle.y = mainNode.y
-                        vehicle.z = mainNode.z
-                        vehicle.isActive = true
-                        vehicle.isHovering = true
-                        --vehicle.isCurrentlyControllable = true
-                        --vehicle.isControllable = true
-                        --vehicle.isSuspended = true
-                        vehicle.hover = 1
-                        --vehicle.clusterId = blam.object(get_dynamic_player()).clusterId
-                        --[[console_out("locationId " .. tostring(vehicle.locationId))
-                        console_out("hover " .. tostring(vehicle.hover))
-                        console_out("thrust " .. tostring(vehicle.thrust))
-                        console_out("isActive " .. tostring(vehicle.isActive))
-                        console_out("isNotMoving " .. tostring(vehicle.isNotMoving))
-                        console_out("isGhost " .. tostring(vehicle.isGhost))
-                        console_out("isControllable " .. tostring(vehicle.isControllable))
-                        console_out("isCurrentlyControllable " .. tostring(vehicle.isCurrentlyControllable))
-                        console_out("isHovering " .. tostring(vehicle.isHovering))
-                        console_out("isFrozen " .. tostring(vehicle.isFrozen))
-                        console_out("isStationary " .. tostring(vehicle.isStationary))
-                        console_out("isSuspended " .. tostring(vehicle.isSuspended))
-                        console_out(("x: {x}, y: {y}, z: {z}"):template({x = vehicle.x, y = vehicle.y, z = vehicle.z}))]]
-                    end
-                end
-            end
-        end
-    end
-    firefightManager.EachTick()
-    healthManager.EachTick()
 end
 
 local onTickEvent = balltze.event.tick.subscribe(function(event)
