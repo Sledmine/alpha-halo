7f2318071619e92ba7bad134327c43e0fdcefd0d
Lennox Pársec <76707597+LennoxParsec@users.noreply.github.com>
2025-01-03 16:47:12 -0600

    Minor fixes.

--- lua/modules/alpha_halo/gameplay_core/eventsManager.lua
+++ lua/modules/alpha_halo/gameplay_core/eventsManager.lua
@@ -7,7 +7,7 @@ local scriptBlock = require "script".block
 
 local bansheeLivingCount
 local snipersLivingCount
-local mortarLivingCount
+--local mortarLivingCount
 
 -- Esta función es llamada cada que el mapa se carga, desde el firefightManager. Se encarga de iniciar el juego.
 local gameIsOn = false
@@ -24,10 +24,10 @@ function eventsManager.eachTick()
 end
 
 -- Esta función es llamada cada tick, desde el eachTick. Se encarga del timer.
-local randomEventTimer = 30 --3600
+local randomEventTimer = 3600
 local randomEventCountdown = randomEventTimer
 function eventsManager.randomEventTimer()
-    if bansheeLivingCount == 0 or mortarLivingCount == 0 or snipersLivingCount == 0 then
+    if bansheeLivingCount == 0 or snipersLivingCount == 0 then -- or mortarLivingCount == 0 (Mortar is not working)
         if randomEventCountdown > 0 then
             randomEventCountdown = randomEventCountdown - 1
         else
@@ -39,13 +39,13 @@ end
 
 -- Esta función es llamada por randomEventTimer. Se encarga de randomizar el evento.
 function eventsManager.randomEventGenerator()
-    local selectedEvent = math.random(1, 3)
+    local selectedEvent = math.random(1, 2) -- 1, 3 (Mortar is not working)
     if selectedEvent == 1 then
         eventsManager.bansheeEvent()
     elseif selectedEvent == 2 then
         eventsManager.sniperEvent()
-    elseif selectedEvent == 3 then
-        eventsManager.mortarEvent()
+    --elseif selectedEvent == 3 then
+    --    eventsManager.mortarEvent()
     end
 end
 
@@ -76,28 +76,28 @@ function eventsManager.sniperEvent()
 end
 
 -- Esta función es llamada desde el randomEventGenerator. Se encarga del evento Covenant Mortar.
-function eventsManager.mortarEvent()
-    if mortarLivingCount == 0 then
-        console_out("Mortar event!")
-        hsc.aiSpawn(1, "Covenant_Mortars")
-        hsc.vehicleLoadMagic("mortar_1", "W-gunner", "Covenant_Mortars/mortar_a")
-        hsc.vehicleLoadMagic("mortar_2", "W-gunner", "Covenant_Mortars/mortar_b")
-    else
-        eventsManager.randomEventGenerator()
-    end
-end
+-- Por ahora, esto no se está llamando, dado que el Mortero no dispara en lo absoluto.
+--function eventsManager.mortarEvent()
+--    if mortarLivingCount == 0 then
+--        console_out("Mortar event!")
+--        hsc.aiSpawn(1, "Covenant_Mortars")
+--        hsc.vehicleLoadMagic("mortar_1", "W-gunner", "Covenant_Mortars/mortar_a")
+--        hsc.vehicleLoadMagic("mortar_2", "W-gunner", "Covenant_Mortars/mortar_b")
+--    else
+--        eventsManager.randomEventGenerator()
+--    end
+--end
 
 -- Esta función es llamada cada tick, desde el eachTick. Se encarga de revisar el estado de los squads.
 function eventsManager.aiCheck()
     bansheeLivingCount = hsc.aiLivingCount("Covenant_Banshees", "banshees_living_count")
     snipersLivingCount = hsc.aiLivingCount("Covenant_Snipers", "snipers_living_count")
-    mortarLivingCount = hsc.aiLivingCount("Covenant_Mortars", "mortars_living_count")
+    --mortarLivingCount = hsc.aiLivingCount("Covenant_Mortars", "mortars_living_count")
     hsc.aiMagicallySeePlayers("Covenant_Banshees")
-    hsc.aiAction(1, "Covenant_Banshees")
     hsc.aiMagicallySeePlayers("Covenant_Snipers")
     hsc.aiAction(1, "Covenant_Snipers")
-    hsc.aiMagicallySeePlayers("Covenant_Mortars")
-    hsc.aiAction(1, "Covenant_Mortars")
+    --hsc.aiMagicallySeePlayers("Covenant_Mortars")
+    --hsc.aiAction(1, "Covenant_Mortars")
 end
 
 return eventsManager
