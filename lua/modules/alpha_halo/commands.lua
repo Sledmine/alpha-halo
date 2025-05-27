5731ae60c1b41965fb735555bdeb4fdeee9006c7
markmcfuzz <magecape10@gmail.com>
2025-05-27 02:42:38 -0600

    add commands module

---
+++ lua/modules/alpha_halo/commands.lua
@@ -0,0 +1,18 @@
+local luna = require "luna"
+local skullsManager = require "alpha_halo.gameplay_core.skullsManager"
+
+local commands = {}
+
+commands = {
+    activate_silver_skull = {
+        description = "Activate a silver skull by name.",
+        help = [[<skull_name | "random">]],
+        minArgs = 1,
+        maxArgs = 1,
+        func = function(skullName)
+            skullsManager.activateSilverSkulls(skullName)
+        end
+    },
+}
+
+return commands
