69c54c642244168a345249043e3a268925ee8bda
markmcfuzz <magecape10@gmail.com>
2025-05-28 15:26:22 -0600

    add new skull commands

--- lua/modules/alpha_halo/commands.lua
+++ lua/modules/alpha_halo/commands.lua
@@ -4,13 +4,24 @@ local skullsManager = require "alpha_halo.gameplay_core.skullsManager"
 local commands = {}
 
 commands = {
-    activate_silver_skull = {
-        description = "Activate a silver skull by name.",
-        help = [[<skull_name | "random">]],
-        minArgs = 1,
-        maxArgs = 1,
-        func = function(skullName)
-            skullsManager.activateSilverSkull(skullName)
+    activate_skull = {
+        description = "Activate a skull by <type> and <arg>.",
+        category = "debug",
+        help = "Usage: activate_skull  [ <silver> | <golden> ]  [ <name> | <random> | <all> ]",
+        minArgs = 2,
+        maxArgs = 2,
+        func = function(type, skullName)
+            skullsManager.enableSkull(type, skullName)
+        end
+    },
+    deactivate_skull = {
+        description = "Deactivate a skull by <type> and <arg>.",
+        category = "debug",
+        help = "Usage: deactivate_skull  [ <silver> | <golden> ]  [ <name> | <random> | <all> | <is_active> ]",
+        minArgs = 2,
+        maxArgs = 2,
+        func = function(type, skullName)
+            skullsManager.disableSkull(type, skullName)
         end
     },
 }
