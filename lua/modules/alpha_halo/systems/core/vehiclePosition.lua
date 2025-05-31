5632bff5526a4f6eda3703e48a5fc0e09ff54834
markmcfuzz <magecape10@gmail.com>
2025-05-31 05:14:56 -0600

    Balltze migration functions

--- lua/modules/alpha_halo/systems/core/vehiclePosition.lua
+++ lua/modules/alpha_halo/systems/core/vehiclePosition.lua
@@ -1,55 +1,46 @@
+local balltze = Balltze
+local engine = Engine
+local getObject = Engine.gameState.getObject
+local objectTypes = Engine.tag.objectType
+local tagClasses = Engine.tag.classes
 local blam = require "blam"
-
 
 local vehiclePosition = {}
 
--- Function to get the position of a node from a vehicleObjectHandle 
+-- THIS IS PROBABLY NOT ACCURATE, BUT WORKS
+-- TRUST ME, I'M SLED DA FOKIN GOAT
+local function getNodePosition(address)
+    local read_float = balltze.memory.readFloat
+    local x = read_float(address + 0x30)
+    local y = read_float(address + 0x34)
+    local z = read_float(address + 0x38)
+    return {x = x, y = y, z = z}
+end
+
 function vehiclePosition.positionUpdater()
-    for objectHandle in pairs(blam.getObjects()) do
-        local object = blam.getObject(objectHandle)
-        if object and object.class == blam.objectClasses.vehicle then
-            local objectTag = blam.getTag(object.tagId)
-            if objectTag then
-                --if objectTag.path:includes("ghost") then
-                if objectTag.path:includes("covenant_spirit") then
-                    local vehicle = blam.vehicle(get_object(objectHandle))
-                    if vehicle then
-                        --execute_script("cls")
-                        local mainNode = getNodePosition(get_object(objectHandle) + 0x5B8)
-                        local absoluteNodeX = mainNode.x + (vehicle.x - mainNode.x)
-                        local absoluteNodeY = mainNode.y + (vehicle.y - mainNode.y)
-                        local absoluteNodeZ = mainNode.z + (vehicle.z - mainNode.z)
-                        --[[console_out(mainNode.x)
-                        console_out(mainNode.y)
-                        console_out(mainNode.z)
-                        console_out("-----------")
-                        console_out(absoluteNodeX)
-                        console_out(absoluteNodeY)
-                        console_out(absoluteNodeZ)]]
-                        vehicle.x = mainNode.x
-                        vehicle.y = mainNode.y
-                        vehicle.z = mainNode.z
-                        vehicle.isActive = true
-                        vehicle.isHovering = true
-                        --vehicle.isCurrentlyControllable = true
-                        --vehicle.isControllable = true
-                        --vehicle.isSuspended = true
-                        vehicle.hover = 1
-                        --vehicle.clusterId = blam.object(get_dynamic_player()).clusterId
-                        --[[console_out("locationId " .. tostring(vehicle.locationId))
-                        console_out("hover " .. tostring(vehicle.hover))
-                        console_out("thrust " .. tostring(vehicle.thrust))
-                        console_out("isActive " .. tostring(vehicle.isActive))
-                        console_out("isNotMoving " .. tostring(vehicle.isNotMoving))
-                        console_out("isGhost " .. tostring(vehicle.isGhost))
-                        console_out("isControllable " .. tostring(vehicle.isControllable))
-                        console_out("isCurrentlyControllable " .. tostring(vehicle.isCurrentlyControllable))
-                        console_out("isHovering " .. tostring(vehicle.isHovering))
-                        console_out("isFrozen " .. tostring(vehicle.isFrozen))
-                        console_out("isStationary " .. tostring(vehicle.isStationary))
-                        console_out("isSuspended " .. tostring(vehicle.isSuspended))
-                        console_out(("x: {x}, y: {y}, z: {z}"):template({x = vehicle.x, y = vehicle.y, z = vehicle.z}))]]
-                    end
+    for vehicleIndex = 0, 4095 do
+        local vehicleObject = getObject(vehicleIndex)
+        if vehicleObject then
+            if vehicleObject.type == objectTypes.vehicle then
+                local vehicle = getObject(vehicleIndex, objectTypes.vehicle)
+                assert(vehicle, "Failed to get vehicle object")
+                local vehicleTag = engine.tag.getTag(vehicleObject.tagHandle.value, tagClasses.vehicle)
+                assert(vehicleTag, "Failed to get scenery tag")
+                if vehicleTag.path:includes("covenant_spirit") then
+                    -- Logger:debug("Vehicle tag found: " .. vehicleTag.path)
+                    local mainNode = getNodePosition(get_object(vehicleIndex) + 0x5B8)
+                    local absoluteNodeX = mainNode.x + (vehicleObject.position.x - mainNode.x)
+                    local absoluteNodeY = mainNode.y + (vehicleObject.position.y - mainNode.y)
+                    local absoluteNodeZ = mainNode.z + (vehicleObject.position.z - mainNode.z)
+                    -- Update the vehicle position
+                    vehicleObject.position.x = mainNode.x
+                    vehicleObject.position.y = mainNode.y
+                    vehicleObject.position.z = mainNode.z
+                    -- console_out(("x: {x}, y: {y}, z: {z}"):template({
+                    --     x = vehicleObject.position.x,
+                    --     y = vehicleObject.position.y,
+                    --     z = vehicleObject.position.z
+                    -- })) -- Logger:debug("Vehicle tag found: " .. vehicleTag.path)
                 end
             end
         end
