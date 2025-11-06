408d35132c9c6d0c028665ee2c28a2797d1f9317
Sledmine <40512223+Sledmine@users.noreply.github.com>
2025-11-06 00:31:43 -0600

    Refactor sputnik skull effect: adjust impulse pushback calculation and clean up gravity handling

--- lua/modules/alpha_halo/systems/gameplay/skulls/sputnik.lua
+++ lua/modules/alpha_halo/systems/gameplay/skulls/sputnik.lua
@@ -1,6 +1,10 @@
 local tagEntries = require "alpha_halo.systems.core.tagEntries"
+local blam = require "blam2"
 
 local sputnik = {}
+
+--local defaultGravity = blam.globalGravity()
+local defaultGravity = 0.00356518
 
 ---Sputnik: Halves gravity and adds acceleration to firing responses.
 ---@param isActive boolean
@@ -8,18 +12,18 @@ function sputnik.skullEffect(isActive, totalSkullPower)
     local finalSkullPower = totalSkullPower or 1
     for _, tagEntry in ipairs(tagEntries.triggerDamageEffect()) do
         local damageEffect = tagEntry.data
-        local impulsePushback = (damageEffect.temporaryCameraImpulsePushback * 10)
+        local impulsePushback = (damageEffect.temporaryCameraImpulsePushback * 3)
         local accelerationMultiplier = impulsePushback * finalSkullPower
         if isActive then
             damageEffect.damageInstantaneousAcceleration.i = damageEffect.damageInstantaneousAcceleration.i + accelerationMultiplier
             damageEffect.damageFlags:skipsShields(true)
             damageEffect.damageUpperBound[2] = damageEffect.damageUpperBound[2] + 0.001 -- This should not increase by any means.
+            blam.globalGravity(defaultGravity / (2 * finalSkullPower))
         else
             Balltze.features.reloadTagData(tagEntry.handle)
+            blam.globalGravity(defaultGravity)
         end
     end
-    -- skullsManager.skulls.sputnik.active = isActive
-    -- logger:debug("Sputnik {}", isActive and "On" or "Off")
 end
 
 return sputnik
