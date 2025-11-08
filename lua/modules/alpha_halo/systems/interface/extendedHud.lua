bbf2e7b51bb4797a5b97bacedf0c30afed806134
markmcfuzz <magecape10@gmail.com>
2025-11-08 09:17:26 -0600

    Add extended HUD functionality: Implement hideMetersOnZoom method for beam rifle

---
+++ lua/modules/alpha_halo/systems/interface/extendedHud.lua
@@ -0,0 +1,40 @@
+local balltze = Balltze
+local engine = Engine
+local objectTypes = Engine.tag.objectType
+local getObject = Engine.gameState.getObject
+local getPlayer = Engine.gameState.getPlayer
+local const = require "alpha_halo.systems.core.constants"
+
+local extendedHud = {}
+
+function extendedHud.hideMetersOnZoom()
+
+    local player = getPlayer()
+    if not player then
+        return
+    end
+    local biped = getObject(player.objectHandle, objectTypes.biped)
+    if not biped then
+        return
+    end
+
+    if not const.hud.extBeamRifle then
+        logger:warning("Extended Beam Rifle HUD not found, skipping hide meters on zoom")
+        return
+    end
+
+    local hudMetersBeamRifle = const.hud.extBeamRifle.data
+    local levelZoom1 = biped.desiredZoomLevel == 0
+    local levelZoom2 = biped.desiredZoomLevel == 1
+
+    for i = 1, hudMetersBeamRifle.meterElements.count do
+        local meterElement = hudMetersBeamRifle.meterElements.elements[i]
+        if levelZoom1 or levelZoom2 then
+            meterElement.anchorOffset.y = 0
+        else
+            meterElement.anchorOffset.y = 1000
+        end
+    end
+end
+
+return extendedHud
