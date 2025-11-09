ec4c64393b204d93450b6a259fb40861bf9eeca0
markmcfuzz <magecape10@gmail.com>
2025-11-09 16:59:45 -0600

    Refactor hideMetersOnZoom function: Consolidate zoom level checks and enhance meter visibility logic for beam and sniper rifles

--- lua/modules/alpha_halo/systems/interface/extendedHud.lua
+++ lua/modules/alpha_halo/systems/interface/extendedHud.lua
@@ -18,14 +18,19 @@ function extendedHud.hideMetersOnZoom()
         return
     end
 
+    local levelZoom1 = biped.desiredZoomLevel == 0
+    local levelZoom2 = biped.desiredZoomLevel == 1
+
+    -------------------------------------------------------
+    -- Beam Rifle
+    -------------------------------------------------------
+
     if not const.hud.extBeamRifle then
         logger:warning("Extended Beam Rifle HUD not found, skipping hide meters on zoom")
         return
     end
 
     local hudMetersBeamRifle = const.hud.extBeamRifle.data
-    local levelZoom1 = biped.desiredZoomLevel == 0
-    local levelZoom2 = biped.desiredZoomLevel == 1
 
     for i = 1, hudMetersBeamRifle.meterElements.count do
         local meterElement = hudMetersBeamRifle.meterElements.elements[i]
@@ -35,6 +40,27 @@ function extendedHud.hideMetersOnZoom()
             meterElement.anchorOffset.y = 1000
         end
     end
+
+    -------------------------------------------------------
+    -- Sniper rifle 
+    -------------------------------------------------------
+
+    if not const.hud.extSniperRifle then
+        logger:warning("Extended Sniper Rifle HUD not found, skipping hide meters on zoom")
+        return
+    end
+
+    local hudMetersSniperRifle = const.hud.extSniperRifle.data
+
+    for i = 1, hudMetersSniperRifle.meterElements.count do
+        local meterElement = hudMetersSniperRifle.meterElements.elements[i]
+        if levelZoom1 or levelZoom2 then
+            meterElement.anchorOffset.y = 100
+        else
+            meterElement.anchorOffset.y = 1000
+        end
+    end
+
 end
 
 return extendedHud
