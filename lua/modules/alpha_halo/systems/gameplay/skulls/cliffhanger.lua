ec93bf8a33a577598185941581bf2b5bd37b4ad9
Lennox Pársec <angelonassire@gmail.com>
2026-06-14 21:29:33 -0600

    Added additional polishment to Cliffhanger. No, projectiles it doesn't attach to everything. Yes, i've double checked.

--- lua/modules/alpha_halo/systems/gameplay/skulls/cliffhanger.lua
+++ lua/modules/alpha_halo/systems/gameplay/skulls/cliffhanger.lua
@@ -11,13 +11,24 @@ function cliffhanger.skullEffect(isActive)
             if not projectile.effect.tagHandle:isNull() then
                 projectile.aiPerceptionRadius = projectile.aiPerceptionRadius + 1
                 projectile.dangerRadius = projectile.dangerRadius + 1
+                if projectile.detonationTimerStarts == 0 then -- Los timers Inmediatly afectan el propostio de Cliffhanger; los cambiamos a When at rest.
+                    ---@diagnostic disable-next-line: assign-type-mismatch
+                    projectile.detonationTimerStarts = 2
+                end
                 ---@diagnostic disable-next-line: assign-type-mismatch
-                projectile.timer = {0.5, 5}
+                projectile.timer = {2, 10}
                 for i = 1, projectile.materialResponse.count do
                     local materialResponse = projectile.materialResponse.elements[i]
-                    if materialResponse.defaultResponse ~= 2 then
-                        materialResponse.defaultResponse = 4 -- 4 = attach
+                    local potentialChance = (materialResponse.potentialBetween[2] > 0) or (materialResponse.potentialAnd[2] > 0) -- Revisamos si hay una chance de que se ejecute el potentialResponse en primer lugar.
+                    if materialResponse.defaultResponse == 0 or materialResponse.defaultResponse == 1 then -- Si es Disappear o Detonate, lo cambiamos a Attach. Dejamos Overpenetrate y Bounce funcionando.
+                        materialResponse.defaultResponse = 4
                     end
+                    if potentialChance and (materialResponse.potentialResponse == 0 or materialResponse.potentialResponse == 1) then -- Si hay una chance y es Disappear o Detonate, lo cambiamos a Attach.
+                        materialResponse.potentialResponse = 4
+                    end
+                    --if materialResponse.defaultResponse ~= 2 then
+                    --    materialResponse.defaultResponse = 4 -- 4 = attach
+                    --end
                 end
             end
         else
